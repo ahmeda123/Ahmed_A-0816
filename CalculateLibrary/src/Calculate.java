85eec4a7eb31fc9c2684be0c415334dc084133f7
ahmeda123 <ahmedalsabbah@gmail.com>
2016-09-06 09:27:04 -0700

    .

--- CalculateLibrary/src/Calculate.java
+++ CalculateLibrary/src/Calculate.java
@@ -14,39 +14,33 @@ public class Calculate {
 	}
 	
 	public static double average (double n1, double n2) {
-		return((n1 + n2) / 2)
+		return((n1 + n2) / 2);
 	}
 	
 	public static double average (double n1, double n2, double n3) {
-		return((n1 + n2 + n3) / 3)
+		return((n1 + n2 + n3) / 3);
     }
 	
-	public static double toDegrees (double number) {
-		return()
+	public static double toDegrees (double radian) {
+		return(180 / 3.14159 * radian);
     }
 	
-	public static double toRadians (double number) {
-		return()
+	public static double toRadians (double degree) {
+		return(3.14159 / 180 * degree);
     }
 	
-	public static double discriminant (double number) {
-		return()
+	public static double discriminant (double b, double a, double c) {
+		return((b * b) - 4 * a * c);
     }
 	
-	public static double average (double number) {
-		return()
-    }
+	public static int toImproperFrac (int n1, int nu, int de {
+		return((n1 * de + nu) / de)
 	
-	public static double toImproperFrac (double number) {
-		return()
+	public static int toMixedNum (int n1, int n2) {
+		return(7 / 2);
 	}
 	
-	public static double toMixedNum (double number) {
-		return()
-	}
-	
-	public static double foil (double number) {
-		return()
+	public static double foil (int n1, int n2, int n3, int n4)
+		return();
     }
-	}
 }
