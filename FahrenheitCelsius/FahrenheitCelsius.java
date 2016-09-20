96b4ea906385be0c3589b4a61b06abb12c13ebbd
ahmeda123 <ahmedalsabbah@gmail.com>
2016-09-20 09:27:19 -0700

    Librarieis
    .

---
+++ FahrenheitCelsius/FahrenheitCelsius.java
@@ -0,0 +1,17 @@
+
+/* @author: Ahmed Alsabbah
+ * Date: 08/30/16
+ * Period 1
+ */
+
+public class FahrenheitCelsius { //converts fahrenheit to celsius
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	double f = 30; // pick any value for f
+	double c = (5.0 / 9) * (f - 32);
+	System.out.println(c);
+
+	}
+
+}
