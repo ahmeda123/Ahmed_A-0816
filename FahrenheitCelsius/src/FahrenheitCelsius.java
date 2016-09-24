a393e49395fa8ca3eaca3cef68f104671e75682f
ahmeda123 <ahmedalsabbah@gmail.com>
2016-09-24 08:31:57 -0700

    Update FahrenheitCelsius.java

--- FahrenheitCelsius/src/FahrenheitCelsius.java
+++ FahrenheitCelsius/src/FahrenheitCelsius.java
@@ -4,11 +4,11 @@
  * Period 1
  */
 
-public class FahrenheitCelsius {
+public class FahrenheitCelsius { //converts fahrenheit to celsius
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	double f = 30;
+	double f = 30; //put anything you want for f
 	double c = (5.0 / 9) * (f - 32);
 	System.out.println(c);
 
