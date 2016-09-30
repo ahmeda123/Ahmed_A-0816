af90bb29977994082cc0f6b897d4af7d8afd7487
ahmeda123 <ahmedalsabbah@gmail.com>
2016-09-30 08:54:52 -0700

    Processing

---
+++ CollaborativeProgramming/src/CollaborativeProgramming.java
@@ -0,0 +1,14 @@
+/*@author Ahmed Alsabbah
+date: 08/30/16
+period 1
+*/
+
+import java.util.*; //importing java util for scanners
+
+public class CollaborativeProgramming { //collaborative code for printing smallest and largest of string of numbers, and printing sum of even numbers
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+	}
+
+}
